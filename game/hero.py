30808e319bcf30c0908bd8a967b58840a67076ec
qwickq <ivan2008dmitriev@yandex.ru>
2022-10-09 02:45:58 +0300

    Поправки под линтера.
    Был произведён рефакторинг кода)

--- game/hero.py
+++ game/hero.py
@@ -7,12 +7,12 @@ from arcade import Sprite, SpriteList
 from constants import (
     HERO_JUMP,
     HERO_PLATFORMJUMP,
-    HERO_TRAMPOLINE,
     HERO_SCALE,
     HERO_SLOWDOWN,
     HERO_SPEED,
     HERO_START_X,
     HERO_START_Y,
+    HERO_TRAMPOLINE
 )
 from game_platforms import PlatformJump, SimplePlatform, Trampoline
 
